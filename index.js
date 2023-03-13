06505033f1ea37a2984c43cc5d515597ed37623c
edd-ie <ogendoeddie@gmail.com>
2023-03-13 12:18:49 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+// companyName: is Scuber as a const.
+const companyName = 'Scuber';
+
+// mostProfitableNeighborhood: is Chelsea as let
+let mostProfitableNeighborhood = 'Chelsea';
+
+//companyCeo: Susan Smith as let
+let companyCeo = 'Susan Smith';
